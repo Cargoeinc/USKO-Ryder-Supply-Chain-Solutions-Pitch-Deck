4ef6b7d023eb2387d8448e5d6a5f37add71288fd
Cargoeinc <info@cargoe.io>
2025-06-12 15:35:34 -0700

    Enable Vercel Analytics debug mode and add console logging

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import { Analytics } from '@vercel/analytics/react';
 
+console.log('Vercel Analytics loaded');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics />
+    <Analytics debug={true} />
   </React.StrictMode>
 ); 
